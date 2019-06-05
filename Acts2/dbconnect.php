38a4dd88002308d437a79716d32e82fc2b734489
hawkeyeaz1 <hawkeyeaz1@gmail.com>
2019-06-04 23:37:19 -0400

    Update dbconnect.php

--- Acts2/dbconnect.php
+++ Acts2/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'purel9_acts2');
+define('DB_USERNAME', 'acts2');
 define('DB_PASSWORD', 'redacted');
-define('DB_DATABASE', 'purel9_acts2db');
+define('DB_DATABASE', 'acts2db');
 ?>
